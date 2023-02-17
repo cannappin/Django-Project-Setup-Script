75df9cf38b76e3559fc3b2b1cff965edc737ec11
Gokool <87528956+cannappin@users.noreply.github.com>
2023-02-17 03:21:21 +0100

    Update django_setup.bat

--- django_setup.bat
+++ django_setup.bat
@@ -1,23 +1,30 @@
 @echo off
 
-set /p base_dir="Enter the name of the base directory where everything will be stored: "
+set /p base_dir_name="Enter the name of the base directory: "
 set /p env_name="Enter the name of the virtual environment: "
 set /p project_name="Enter the name of the Django project: "
-set /p github_repo="Enter the GitHub repository URL: "
+set /p repo_url="Enter the URL of your GitHub repository: "
 
 if not exist %base_dir% mkdir %base_dir%
-cd %base_dir%
+cd %base_dir_name%
 
 python -m venv %env_name%
 call %env_name%\Scripts\activate.bat
 pip install django
-django-admin startproject %project_name%
-cd %project_name%
+django-admin startproject %project_name% .
+echo Django project '%project_name%'has been created in directory '%cd%'.
 git init
-git remote add origin %github_repo%
+echo /%env_name%/* > .gitignore
+git remote add origin %repo_url%
 git pull origin master
 git add .
 git commit -m "Initial commit"
 git push -u origin master
 
-start "" "C:\Program Files\Git\git-bash.exe" -c "python manage.py runserver"
+set /p run_server="Do you want to run the development server now? (y/n): "
+
+if /i "%run_server%"=="y" (
+    python manage.py runserver
+)
+
+
